f6ae9b5656f7d52598bccdab61305b01b1b343d8
flopsanb <flopsanb@g.educaand.es>
2025-06-09 17:42:05 +0200

    Frontend online

--- src/environments/environments.prod.ts
+++ src/environments/environments.prod.ts
@@ -2,5 +2,5 @@ export const environments = {
   production: true,
 };
 
-export const URL_BASE = 'http://localhost:8000';
+export const URL_BASE = 'https://voltfencerbackend-production.up.railway.app';
 export const URL_API = `${URL_BASE}/api/private`;
